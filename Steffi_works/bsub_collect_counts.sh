9c527786aac5ea9154014714e0e27853199b718b
Siwei Zhang <siwei.zhang84@gmail.com>
2026-06-11 01:50:04 -0500

    added a down_sampling function

--- Steffi_works/bsub_collect_counts.sh
+++ Steffi_works/bsub_collect_counts.sh
@@ -8,7 +8,7 @@ mkdir -p log
 #BSUB -J "run_bsub_count_size"
 
 #BSUB -o log/run_bsub_count_size.out.%J
-#BSUB -e log/err.%J
+#BSUB -e log/run_bsub_count_size.err.%J
 
 module load conda3/202402
 conda activate /research_jude/rgs01_jude/groups/cab/projects/automapper/common/szhang37/standalone_conda_envs/r45_py312_scARC
@@ -16,4 +16,4 @@ conda activate /research_jude/rgs01_jude/groups/cab/projects/automapper/common/s
 working_dir="/research_jude/rgs01_jude/groups/cab/projects/automapper/common/szhang37/pulled_git_repos/Multiome_main/Steffi_works"
 cd $working_dir || exit 1
 
-Rscript read_vcf_and_sampling.R
+Rscript ./read_vcf_and_sampling.R
--- Steffi_works/read_vcf_and_sampling.R
+++ Steffi_works/read_vcf_and_sampling.R
@@ -237,8 +237,7 @@ read_tsv_gz_file <-
         refCount > 0,
         altCount > 0
       ) |>
-      dplyr::select(1:7) |>
-      dplyr::mutate(totalCount = refCount + altCount)
+      dplyr::select(1:6)
     return(as.data.frame(df_raw))
   }
 
@@ -247,8 +246,7 @@ read_tsv_gz_file <-
 tsv_df_list <-
   foreach(
     tsv_file = main_tsv_bam_table$tsv_file,
-    .packages = "readr",
-    .export = "read_tsv_gz_file"
+    .packages = "readr"
   ) %dopar%
   {
     read_tsv_gz_file(tsv_file)
@@ -266,5 +264,45 @@ qs_save(
 qs_save(
   main_tsv_bam_table,
   file = file.path(data_dir, "main_tsv_bam_table.qs2"),
+  nthreads = 8
+)
+
+normalised_df_tsv_list <-
+  lapply(
+    names(tsv_df_list),
+    function(sample_id) {
+      df <- tsv_df_list[[sample_id]]
+      df$inverse_size <- main_tsv_bam_table$inverse_size[main_tsv_bam_table$sample_id == sample_id]
+      df$norm_refCount <- round(df$refCount * df$inverse_size)
+      df$norm_altCount <- round(df$altCount * df$inverse_size)
+      # df$norm_totalCount <- 
+      #   df$norm_refCount + 
+      #   df$norm_altCount
+      df$one_line_index <- paste0(
+        df$contig, 
+        ":", 
+        df$position, 
+        ":", 
+        df$variantID, 
+        ":",
+        df$refAllele, 
+        ":", 
+        df$altAllele
+      )
+      df_2_return <- 
+      df[, 
+      c(
+        "one_line_index", 
+        "norm_refCount", 
+        "norm_altCount"
+        )]
+      return(df_2_return)
+    }
+  )
+names(normalised_df_tsv_list) <-
+  names(tsv_df_list)
+qs_save(
+  normalised_df_tsv_list,
+  file = file.path(data_dir, "normalised_df_tsv_list.qs2"),
   nthreads = 8
 )
